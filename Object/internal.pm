f9ac0e83e1824d2c4b05efea3a0146d408194b02
shlomif <shlomif@6f433eb9-a002-0410-945b-ee5462c261ee>
2006-03-07 19:29:49 +0000

    Relicensed the program as dual GPL/Clarified artistic.

--- Object/internal.pm
+++ Object/internal.pm
@@ -1,20 +1,8 @@
-# $Id: internal.pm,v 1.4 2005/06/19 14:24:00 nanardon Exp $
+# $Id$
 
 #- Olivier Thauvin <olivier.thauvin@aerov.jussieu.fr>
 
-#- This program is free software; you can redistribute it and/or modify
-#- it under the terms of the GNU General Public License as published by
-#- the Free Software Foundation; either version 2, or (at your option)
-#- any later version.
-#-
-#- This program is distributed in the hope that it will be useful,
-#- but WITHOUT ANY WARRANTY; without even the implied warranty of
-#- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#- GNU General Public License for more details.
-#-
-#- You should have received a copy of the GNU General Public License
-#- along with this program; if not, write to the Free Software
-#- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# This program is free software distributed under the same terms as Parrot.
 
 package File::Find::Object::internal;
 
