64371c16075ca0a425571096a8ef0f60bad66bd3
shlomif <shlomif@6f433eb9-a002-0410-945b-ee5462c261ee>
2006-08-23 10:51:55 +0000

    Moved File::Find::Object::internal inside the File/Find/Object.pm module, for convenience.

--- lib/File/Find/Object.pm
+++ lib/File/Find/Object.pm
@@ -5,14 +5,45 @@
 # This program is free software, distributed under the same terms as 
 # Parrot.
 
+package File::Find::Object::internal;
+
+use strict;
+use warnings;
+
+use File::Spec;
+sub _curr_file
+{
+    my $self = shift;
+
+    if (@_)
+    {
+        $self->{_curr_file} = shift;
+    }
+
+    return $self->{_curr_file};
+}
+
+sub new {
+    my ($class, $top, $from, $index) = @_;
+
+    my $self = {
+        dir => $top->current_path($from),
+        idx => $index,
+    };
+
+    bless($self, $class);
+
+    $from->{dir} = $self->{dir};
+
+    return $top->open_dir($top->_father($self)) ? $self : undef;
+}
+
 package File::Find::Object;
 
 use strict;
 use warnings;
 
 use Carp;
-
-use File::Find::Object::internal;
 
 our $VERSION = '0.0.4';
 
