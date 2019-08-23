24496e49d5109f6c0669a3b3d5f0a41f033baad5
Shlomi Fish <shlomif@shlomifish.org>
2019-08-23 18:37:43 +0300

    from bitbucket-hg to github due to sunsetting

--- scripts/tag-release.pl
+++ scripts/tag-release.pl
@@ -6,22 +6,18 @@ use warnings;
 use IO::All;
 
 my ($version) =
-    ( map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-        io->file('lib/File/Find/Object.pm')->getlines() );
+    ( map { m{\Aversion * = *(\S+)} ? ($1) : () }
+        io->file("./dist.ini")->getlines() );
 
 if ( !defined($version) )
 {
     die "Version is undefined!";
 }
 
-my $mini_repos_base =
-    'https://svn.berlios.de/svnroot/repos/web-cpan/XML-Grammar-Fortune';
-
 my @cmd = (
-    "hg", "tag", "-m", "Tagging the File-Find-Object release as $version",
+    "git", "tag", "-m", "Tagging the File-Find-Object release as $version",
     "releases/$version",
 );
 
 print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
 exec(@cmd);
-
